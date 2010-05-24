c2183f66457dda41e5bf102e9f6c2ae1e523b1d8
AlexV <asmodehn@gmail.com>
2010-05-24 13:33:17 +0000

    Adding Install and Package feature. STILL NOTE READY FOR PRODUCTION

--- dist/WkPack.cmake
+++ dist/WkPack.cmake
@@ -1,5 +1,5 @@
 # 
-# Copyright (c) 2009, Asmodehn's Corp.
+# Copyright (c) 2010, Asmodehn's Corp.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without 
@@ -28,36 +28,49 @@
 #
 
 #debug
-message ( STATUS "== Loading WkInstall.cmake ..." )
+message ( STATUS "== Loading WkPack.cmake ..." )
 
 if ( CMAKE_BACKWARDS_COMPATIBILITY LESS 2.6 )
 	message ( FATAL_ERROR " CMAKE MINIMUM BACKWARD COMPATIBILITY REQUIRED : 2.6 !" )
 endif( CMAKE_BACKWARDS_COMPATIBILITY LESS 2.6 )
 
-
 #
-# Defining installation rules based on well known hierarchy
+# Defining package rules based on well known hierarchy
 #
 
-MACRO (WkInstall)
+macro( WkPack )
 
-	INSTALL (
-		DIRECTORY ${CMAKE_BINARY_DIR}/${WKCMAKE_INCLUDE_DIR}/${PROJECT_NAME} DESTINATION include 
-		PATTERN ".svn" EXCLUDE)
+INCLUDE(InstallRequiredSystemLibraries)
 
-	INSTALL	(
-		TARGETS ${PROJECT_NAME} EXPORT ${PROJECT_NAME}
-		RUNTIME DESTINATION bin
-		LIBRARY DESTINATION lib/${PROJECT_NAME}
-		ARCHIVE DESTINATION lib/${PROJECT_NAME}
-		)
+SET ( CPACK_PACKAGE_DESCRIPTION_SUMMARY "Test Package" )
+SET(CPACK_PACKAGE_VENDOR "Asmodehn's Corp.")
 
-	INSTALL (
-		EXPORT ${PROJECT_NAME} DESTINATION WkCMake
-		FILE ${PROJECT_NAME}Export.cmake
-		)
-	
-	INSTALL (
-		FILES ${CMAKE_BINARY_DIR}/${PROJECT_NAME}Config.cmake DESTINATION WkCMake
-		)
-ENDMACRO(WkInstall)
+#TODO : Later : Force existence of these 2 files
+#( see GNU standard, etc. )
+#SET(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/ReadMe.txt")
+#SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/Copyright.txt")
+
+#TODO : Gest version from CMake config / SVN Revision / etc. )
+SET(CPACK_PACKAGE_VERSION_MAJOR "0")
+SET(CPACK_PACKAGE_VERSION_MINOR "0")
+SET(CPACK_PACKAGE_VERSION_PATCH "1")
+#default install location on linux : /opt
+SET(CPACK_PACKAGE_INSTALL_DIRECTORY "CMake ${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}")
+
+if (WIN32 AND NOT UNIX)
+	SET( CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/Cmake\\\\InstallIcon.bmp")
+	SET(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\Target.exe")
+	SET(CPACK_NSIS_DISPLAY_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY} My Prject")
+	SET(CPACK_NSIS_HELP_LINK "http://asmodehn.com")
+	SET(CPACK_NSIS_URL_INFO_ABOUT "http://asmodehn.com")
+	SET(CPACK_NSIS_CONTACT "alex@asmodehn.com")
+
+endif (WIN32 AND NOT UNIX)
+
+INCLUDE(CPack)
+
+endmacro (WkPack )
+
+
+
+
