7d97ac9fe9245f26414d1148b86dbe804506f40b
AlexV <asmodehn@gmail.com>
2011-09-27 14:41:39 +0000

    fixing build script

--- tests/MySQL++/build.sh
+++ tests/MySQL++/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BUILD_DIR="../MySql_build"
+BUILD_DIR="../MySQL++_build"
 SRC_DIR=`pwd`
 BUILD_TYPE=Release
 ENABLE_TESTS=ON
@@ -11,7 +11,7 @@ mkdir -p $BUILD_DIR && \
 cd $BUILD_DIR
 
 echo "Running in `pwd` :"
-cmake -DMySql_BUILD_TYPE=$BUILD_TYPE -DMySql_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
+cmake -DMySQLpptest_BUILD_TYPE=$BUILD_TYPE -DMySQLpptest_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
 
 make
 
--- tests/MySql/build.sh
+++ tests/MySql/build.sh
@@ -11,7 +11,7 @@ mkdir -p $BUILD_DIR && \
 cd $BUILD_DIR
 
 echo "Running in `pwd` :"
-cmake -DMySql_BUILD_TYPE=$BUILD_TYPE -DMySql_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
+cmake -DMySqltest_BUILD_TYPE=$BUILD_TYPE -DMySqltest_ENABLE_TESTS=$ENABLE_TESTS $SRC_DIR
 
 make
 
